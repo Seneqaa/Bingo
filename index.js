ce6669d7477a54e9181a1a260b27adba020dee1e
Jai Dyelle Dela Cruz <Jaidyelle12@gmail.com>
2024-03-15 08:59:56 +0800

    Committed

---
+++ index.js
@@ -0,0 +1,37 @@
+console.log("Hello World!")
+
+let B = false, I = false, N = false, G = false, O = false;
+
+function newNumber (maxNumber){
+let x = Math.floor(Math.random() * maxNumber) + 1;
+
+
+if(x <= 15){
+    console.log(`New number is ${x} belongs to "B"`);
+    B=true;
+} else if (x >= 16 && x <= 30 ){
+    console.log (`New number is ${x} belongs to "I"`);
+    I= true;
+} else if (x >= 31 && x <=45 ) {
+    console.log (`New number is ${x} belong to "N"`);
+    N=true;
+} else if ( x >= 46 && x  <= 60){
+    console.log (`New number is ${x} belongs to "G"`);
+    G=true;
+}else if ( x >=  61 && x <= 75){
+    console.log (`New Number is ${x} belongs to "0"`);
+    O=true;
+}else{
+    console.log(`New number ${x} is invalid!`);   
+}
+
+if (B = true && I == true && N == true && G == true && O == true){
+    console.log(`BINGOOOO!!!!`);
+}
+
+return console.log(x);
+}
+
+
+
+newNumber(75);
